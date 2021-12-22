106cb794ed4e1ff036a838177cb9aa93cfd5613e
Minu <juty9026@gmail.com>
2021-12-22 17:59:01 +0900

    Modify server configuration

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,3 @@
-// var http = require("http");
-
-// //create a server object:
-// http
-//   .createServer(function(req, res) {
-//     res.write("Hello World!"); //write a response to the client
-//     res.end(); //end the response
-//   })
-//   .listen(8080); //the server object listens on port 8080
-
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const path = require("path");
